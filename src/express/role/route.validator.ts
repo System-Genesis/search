769905de50287782ff858026cd9e2abd6acb29f5
tommyafek <tommyafek@gmail.com>
2021-12-22 12:55:20 +0200

    fix: clearance not in route validator

--- src/express/role/route.validator.ts
+++ src/express/role/route.validator.ts
@@ -1,7 +1,5 @@
 import * as Joi from 'joi';
 
-
-
 export const getSearchRequestSchema = Joi.object({
     query: {
         roleId: Joi.string().required().min(2),
@@ -10,6 +8,7 @@ export const getSearchRequestSchema = Joi.object({
         hierarchy: Joi.alternatives().try(Joi.array(), Joi.string()).allow(Joi.array().length(0)),
         source: Joi.alternatives().try(Joi.array(), Joi.string()).allow(Joi.array().length(0)),
         directGroup: Joi.alternatives().try(Joi.array(), Joi.string()).allow(Joi.array().length(0)),
+        clearance: Joi.alternatives().try(Joi.array(), Joi.string()).allow(Joi.array().length(0)),
     },
 });
 
