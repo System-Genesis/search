2bfbd2dacf286dc0bb77c841414371a244bb51c5
tommyafek <tommyafek@gmail.com>
2022-04-18 12:10:51 +0300

    (fix): resolve conflicts

--- src/express/entity/router.ts
+++ src/express/entity/router.ts
@@ -7,7 +7,12 @@ import ValidateRequest from '../../utils/joi';
 
 const entityRouter: Router = Router();
 // TODO (RN) - Fullname => FullName
-entityRouter.get('/search', ValidateRequest(getSearchRequestSchema), wrapController(ElasticEntityController.searchByFullname));
+entityRouter.get(
+    '/search',
+    ValidateRequest(getSearchRequestSchema),
+    ValidateRequest(validateOneExistence),
+    wrapController(ElasticEntityController.searchByFullname),
+);
 entityRouter.post('/entity', ValidateRequest(getPostRequestSchema), wrapController(ElasticEntityController.postEntityElastic));
 
 export default entityRouter;
